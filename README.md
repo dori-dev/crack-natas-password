ef8961cc47d78ca1a2bc18e611cd518ff4d36e1a
mohammad <mr.dori.dev@gmail.com>
2021-11-18 14:56:17 +0330

    update codes

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 Cracking `natas15` password!
 
 #
+
 # How to Use
 
-## Solving Natas 15 
+## Authentication Natas 15
 
-link: http://natas15.natas.labs.overthewire.org/index.php
+Link: http://natas15.natas.labs.overthewire.org/index.php
+
 Username: natas15
+
 Password: AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J
--- variables.py
+++ variables.py
@@ -1,10 +1,13 @@
 """Project variables
 """
 
+# Third party import
+from requests.auth import HTTPBasicAuth
+
 BASIC_USER = "natas15"
 BASIC_PASS = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
 NAME = 'natas16'
 
-auth = (BASIC_USER, BASIC_PASS)
+auth = HTTPBasicAuth(BASIC_USER, BASIC_PASS)
 URL = "http://natas15.natas.labs.overthewire.org/index.php"
 CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
