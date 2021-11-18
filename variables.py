857a4ccf14e44053bf90425174345cfaf7585fcd
mohammad <mr.dori.dev@gmail.com>
2021-11-18 13:20:55 +0330

    add required

--- variables.py
+++ variables.py
@@ -1,2 +1,13 @@
 """Project variables
 """
+
+# Standart library import
+from string import ascii_letters, digits
+
+basic_user = "natas15"
+basic_pass = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
+auth = (basic_user, basic_pass)
+
+url = "http://natas15.natas.labs.overthewire.org/index.php"
+
+characters = ascii_letters + digits
