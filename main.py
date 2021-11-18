73dee30c2e826901f7aa8e0ea37b758072026c32
mohammad <mr.dori.dev@gmail.com>
2021-11-18 13:16:30 +0330

    add required variables

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ import requests
 from string import ascii_letters, digits
 
 characters = ascii_letters + digits
+password = ""
+url = "http://natas15.natas.labs.overthewire.org/index.php"
+basic_user = "natas15"
+basic_pass = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
